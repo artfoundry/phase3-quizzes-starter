391f5ae5975f0e397ca1c66798329ac8a8a2bced
Dev Bootcamp <Apprentice@dbc34.local>
2013-10-16 16:49:58 -0700

    more javascript magic

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,8 +1,13 @@
-var successCallback = function(data){
+var quizSuccessCallback = function(data){
   $(data.quizzes).each(makeAQuizOnThePage)
 }
 var failCallback = function(xhr){
   alert("Something went wrong!")
+}
+
+var questionSuccessCallback = function(data){
+  var question = new Question(data)
+  putQuestionOnThePage(question.question_link)
 }
 
 var test=false
@@ -13,15 +18,31 @@ $(function(){
   }
   $('.container').empty()
   session_key = {session_key: String(parseInt(Math.random()*1000000))}
-  Controller.getQuizzes(successCallback, failCallback)
+  Controller.getQuizzes(quizSuccessCallback, failCallback)
   listenForClick()
 })
 
+function Question(data){
+  this.question_id = data.id
+  this.question_text = data.question
+}
+
+Question.prototype.appendResponsesToQuestion = function(data){
+  $(data.choices).each(
+    function(key,value){
+
+    }
+  )
+}
 
 
 var Controller = {
   getQuizzes: function(success, fail){
     $.getJSON('/quizzes.json',session_key).success(success).fail(fail)
+  },
+  getQuestions: function(idNo){
+    $.getJSON('/quizzes/' + idNo + '/questions/next.json',session_key).success(questionSuccessCallback)
+      .fail(failCallback)
   }
 }
 
@@ -31,13 +52,21 @@ function listenForClick(){
 
 function respondToClick(evt){
   if (evt.target.id.match(/quiz/)) {
-    alert('call quiz route')}
-  // else if evt.target === question
-  //   call question route
+    Controller.getQuestions(/quiz-(\d+)/.exec(evt.target.id)[1])}
+  else if (evt.target.id.match(/question/)) {
+    alert('call question route')
+
+  }
   // else if evt.target === answer
   //   call answer route
 }
 
 function makeAQuizOnThePage(i,e){
-  $('.container').append("<a id='quiz-"+e.id+"'>" + e.id + ':' + e.name + "</a>")
+  var this_quiz = new Quiz(e)
+  $('.container').append(this_quiz.quiz_link)
+}
+
+function Quiz(quiz_object){
+  this.quiz_link = "<a id='quiz-"+quiz_object.id+"'>" + quiz_object.id + ':' + quiz_object.name + "</a>"
+  this.quiz_id = quiz_object.id
 }
